be508cfea477cddcf01c21f61b3281703ac8aa3f
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2020-01-18 14:05:36 +0100

    Added tests
    Signed-off-by: Dimitris Zlatanidis <d.zlatanidis@gmail.com>

--- tests/test_md5sum.py
+++ tests/test_md5sum.py
@@ -1,6 +1,10 @@
 from slpkg.md5sum import md5
 
 
-def test_md5():
+def test_md5_superuser():
     result = md5('slpkg/superuser.py')
     assert result == "e6cebdf37fbc1b8e9d3c5e3e53b300c1"
+
+def test_md5_security():
+    result = md5('slpkg/security.py')
+    assert result == "d395d2fcf1c7b1a91ef6ce3dc8eb047b"
