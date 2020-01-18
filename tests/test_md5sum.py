0c1ef40bd016c4cbcab1e9c8b5c4cbde0aada06d
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2020-01-18 16:32:29 +0100

    Updated tests
    Signed-off-by: Dimitris Zlatanidis <d.zlatanidis@gmail.com>

--- tests/test_md5sum.py
+++ tests/test_md5sum.py
@@ -1,6 +1,4 @@
 from slpkg.md5sum import md5
-from slpkg.sizes import units
-from slpkg.utils import Utils
 
 
 def test_md5_superuser():
@@ -11,19 +9,3 @@ def test_md5_superuser():
 def test_md5_security():
     result = md5('slpkg/security.py')
     assert result == "d395d2fcf1c7b1a91ef6ce3dc8eb047b"
-
-
-def test_units():
-    assert ["Kb", "Kb"], ["100", "100"] == units(['100', ['100']])
-
-
-def test_dimensional_list():
-    lists = [[1, 2, 3, 4, 5]]
-    utils = Utils()
-    assert [1, 2, 3, 4, 5] == utils.dimensional_list(lists)
-
-
-def test_remove_dbs():
-    lists = [1, 2, 3, 3, 4, 5, 2, 1]
-    utils = Utils()
-    assert [1, 2, 3, 4, 5] == utils.remove_dbs(lists)
